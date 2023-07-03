c6b45b3a098b85e7facbc6123696275c5f52b4b4
sultanowski <sultanowski.d@gmail.com>
2023-07-03 17:58:22 +0200

    pre-release: build

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,1 @@
-"use strict";
-(() => {
-  // bin/live-reload.js
-  new EventSource(`${"http://localhost:3000"}/esbuild`).addEventListener("change", () => location.reload());
-})();
-//# sourceMappingURL=index.js.map
+"use strict";(()=>{})();
